c5064e40f05532d13901b5e28f67a6f439f4fa62
dongmin7208 <sayg3o3d@gmail.com>
2024-09-07 00:57:50 +0900

    ADD:MobileMenu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
@@ -16,27 +17,72 @@ const MobileMenu = () => {
       </button>
 
       {isOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-75 z-50 flex flex-col items-center justify-center text-white">
-          <button
-            onClick={toggleMenu}
-            className="absolute top-4 right-4 text-4xl"
-          >
-            &times;
-          </button>
-          <nav className="space-y-8 text-2xl">
-            <Link href="/categories/culture" onClick={toggleMenu}>
-              문화
-            </Link>
+        <div className="fixed inset-0 bg-[#E6E6E6] flex flex-col pl-6 pr-6 pb-6">
+          {/* 상단 로고 및 닫기 버튼 */}
+          <div className="flex items-center justify-between w-full h-[54px]">
+            <span className="text-[24px] font-bold text-black">dazzi.</span>
+            <button onClick={toggleMenu} className="text-black text-4xl">
+              &times;
+            </button>
+          </div>
+
+          {/* 메뉴 항목 */}
+          <nav className="flex flex-col items-end mt-[24px] space-y-[24px] flex-grow">
             <Link href="/categories/food" onClick={toggleMenu}>
-              음식
+              <span className="block font-bold text-[24px] text-black">
+                음식
+              </span>
             </Link>
             <Link href="/categories/travel" onClick={toggleMenu}>
-              여행
+              <span className="block font-bold text-[24px] text-black">
+                여행
+              </span>
             </Link>
-            <Link href="/categories/hobby" onClick={toggleMenu}>
-              취미
+            <Link href="/categories/daily" onClick={toggleMenu}>
+              <span className="block font-bold text-[24px] text-black">
+                일상
+              </span>
+            </Link>
+            <Link href="/categories/interview" onClick={toggleMenu}>
+              <span className="block font-bold text-[24px] text-black">
+                거리 인터뷰
+              </span>
+            </Link>
+            <Link href="/categories/fashion" onClick={toggleMenu}>
+              <span className="block font-bold text-[24px] text-black">
+                패션
+              </span>
             </Link>
           </nav>
+
+          {/* 하단 아이콘 */}
+          <div className="flex space-x-[16px] mt-auto justify-center w-full">
+            <Link href="https://instagram.com" onClick={toggleMenu}>
+              <Image
+                src="/assets/img/icons/Instagram_R_Clr.webp"
+                width={48}
+                height={48}
+                alt="instagram"
+              />
+            </Link>
+            <Link
+              href="https://youtube.com/@namagawaki1229"
+              onClick={toggleMenu}
+            >
+              <Image
+                src="/assets/img/icons/Youtube_R_Clr.svg"
+                width={36}
+                height={36}
+                alt="instagram"
+                style={{
+                  backgroundColor: 'white',
+                  borderRadius: '50%',
+                  padding: '10px',
+                  width: '48px',
+                }}
+              />
+            </Link>
+          </div>
         </div>
       )}
     </>
