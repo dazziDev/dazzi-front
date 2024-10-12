4b48273bd50e8811616b171a906e7f3025ac97bb
dongmin7208 <sayg3o3d@gmail.com>
2024-10-12 11:41:16 +0900

    ADD: google tag manager

--- src/app/api/articles/fetchArticles.ts
+++ src/app/api/articles/fetchArticles.ts
@@ -4,7 +4,10 @@ import { Category } from '@/app/types/article';
 
 export const fetchArticles = async (): Promise<Category[]> => {
   try {
-    const response = await axios.get('/api/v1/article/list');
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/admin/article/list`
+    );
+    console.log('response', response);
     const rawData = response.data.data;
 
     /// rawData에서 카테고리별로 데이터를 변환
@@ -13,7 +16,7 @@ export const fetchArticles = async (): Promise<Category[]> => {
       articles: rawData[categoryName],
       /// 각 카테고리 내 첫 기사에서 카테고리 ID를 추출
       /// 쓸곳이 있을지는 모르겠지만, 일단 넣어둠
-      categoryId: rawData[categoryName][0].categoryId,
+      categoryId: rawData[categoryName][0]?.categoryId ?? null,
     }));
 
     return categories;
